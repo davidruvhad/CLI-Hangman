330556f1bb52cfd7c3fd7a37e5a82b2a1a71e9ce
davidruvhad <163026852+davidruvhad@users.noreply.github.com>
2026-07-28 04:56:05 -0500

    Add word list for Hangman game
    This file contains a list of words categorized into animals, programming, food, nature, and random themes for use in the Hangman game.

---
+++ words.py
@@ -0,0 +1,26 @@
+"""
+Word list for the Hangman game.
+Feel free to add more words!
+"""
+
+WORD_LIST = [
+    # Animals
+    "elephant", "giraffe", "penguin", "kangaroo", "dolphin",
+    "octopus", "cheetah", "flamingo", "raccoon", "squirrel",
+
+    # Programming
+    "python", "javascript", "algorithm", "function", "variable",
+    "compiler", "database", "framework", "developer", "keyboard",
+
+    # Food
+    "spaghetti", "chocolate", "pineapple", "strawberry", "avocado",
+    "hamburger", "sandwich", "pancake", "broccoli", "cinnamon",
+
+    # Nature
+    "mountain", "waterfall", "rainbow", "volcano", "hurricane",
+    "forest", "desert", "glacier", "meadow", "canyon",
+
+    # Random
+    "adventure", "mystery", "treasure", "journey", "wizard",
+    "dragon", "castle", "guitar", "telescope", "umbrella",
+]
